11075448f4da18160f266b599017507a0fdd44ec
Valentina Rudometova <valentina@rudometova.ru>
2025-12-10 01:37:53 +0300

    back to main

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	defaultArticleState,
-	ArticleStateType,
-} from './constants/articleProps';
+import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -16,37 +13,19 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [articleState, setArticleState] =
-		useState<ArticleStateType>(defaultArticleState);
-	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-	const handleApply = (newState: ArticleStateType) => {
-		setArticleState(newState);
-	};
-
-	const handleReset = () => {
-		setArticleState(defaultArticleState);
-	};
-
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': articleState.fontFamilyOption.value,
-					'--font-size': articleState.fontSizeOption.value,
-					'--font-color': articleState.fontColor.value,
-					'--container-width': articleState.contentWidth.value,
-					'--bg-color': articleState.backgroundColor.value,
+					'--font-family': defaultArticleState.fontFamilyOption.value,
+					'--font-size': defaultArticleState.fontSizeOption.value,
+					'--font-color': defaultArticleState.fontColor.value,
+					'--container-width': defaultArticleState.contentWidth.value,
+					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm
-				isOpen={isSidebarOpen}
-				onToggle={() => setIsSidebarOpen(!isSidebarOpen)}
-				currentState={articleState}
-				onApply={handleApply}
-				onReset={handleReset}
-			/>
+			<ArticleParamsForm />
 			<Article />
 		</main>
 	);
